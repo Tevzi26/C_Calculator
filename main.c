2740263678760ea8ff7369a5d65af5d160dd0c75
Tevzi06 <knez.matevz123@gmail.com>
2023-11-28 19:39:55 +0100

    added sum

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 
 void getString(char *string, int size){
 	printf("Vnesi racun: ");
 	fgets(string, size, stdin);
+
+	int lenght = strlen(string);
+
+	if (lenght > 0 && string[lenght-1] == '\n') string[lenght-1] = '\0';
+}
+
+int handleSum(char *string, int size){
+	int sum = 0;
+	for (int i = 0; i < size; i++){
+		if (string[i] >= '0' && string[i] <= '9' && i < 1){
+			sum = string[i] - '0';
+		}else if (string[i] == '+'){
+			sum = sum + string[i+1] - '0';
+		}
+	}
+	return sum;
 }
 
 
@@ -12,6 +29,8 @@ int main(void){
 	getString(string, sizeof(string));
 
 	printf("%s\n", string);
+	int sum = handleSum(string, strlen(string));
+	printf("%d\n", sum);
 
 
 	return 0;
