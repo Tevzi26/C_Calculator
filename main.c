5f4a9f951d43f22b0dc189e7bf3f903a0f8770ac
Tevzi06 <knez.matevz123@gmail.com>
2023-12-03 14:04:13 +0100

    edited string parser

--- main.c
+++ main.c
@@ -25,17 +25,28 @@ double divide(double a, double b){
 	}
 }
 
-void parseString(char *input){
-	for(int i = 0; input[i]; i++){
-		if (isdigit(input[i])) {
-			int num = atoi(input + i);
-			printf("%d ", num);
+int isOperator(char c){
+	return(c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')');
+}
 
-			while (isdigit(input[i])) ++i;
+void parseString(char *input, double *numbers, char *operators, int *nNum, int *nOp){
+	*nNum = 0;
+	*nOp = 0;
+
+	for(int i = 0; input[i]; ++i){
+		if (isdigit(input[i]) || input[i] == '.' && isdigit(input[i+1] )) {
+			numbers[*nNum] = strtod(input + i, NULL);
+			
+
+			while (isdigit(input[i]) || input[i] == '.') ++i;
 			--i;
 
-		} else if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/' || input[i] == '(' || input[i] == ')') {
-			printf("%c ", input[i]);
+			(*nNum)++;
+
+		} else if(isOperator(input[i])) {
+			operators[*nOp] = input[i];
+
+			(*nOp)++;
 	        }
 	}
 }
@@ -53,9 +64,18 @@ int main(void){
 	char input[100];
 	getString(input, sizeof(input));
 
-	printf("%s\n%d\n", input, strlen(input));
-	parseString(input);
+	double numbers[10];
+	char operators[10];
+	int nNum, nOp;
 
+	parseString(input, numbers, operators, &nNum, &nOp);
 
+	printf("Expression 1:\n");
+	for (int i = 0; i < nNum; ++i)
+		printf("%.2f ", numbers[i]);
+		printf("\n");
+	for (int i = 0; i < nOp; ++i)
+		printf("%c ", operators[i]);
+	printf("\n");
 	return 0;
 }
