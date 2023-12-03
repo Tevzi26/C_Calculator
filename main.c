6410e1b2a66252a6fce97dc19ca26498a85f7713
Tevzi06 <knez.matevz123@gmail.com>
2023-12-03 13:36:00 +0100

    added string parser

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <math.h>
 
 double add(double a, double b){
@@ -23,18 +25,18 @@ double divide(double a, double b){
 	}
 }
 
-void getType(char *input, int *isNum, int lenght){
-	
-	for (int i = 0; i < lenght; i++){
-		if (input[i] <= '9' && input[i] >= '0'){
-			isNum[i] = 1;
-		}
-		else if (input[i] == '+' || input[i] == '-' || input[i] == '/' || input[i] == '*'){
-			isNum[i] = 0;
-		}
-		else{
-			isNum[i] = -1;
-		}
+void parseString(char *input){
+	for(int i = 0; input[i]; i++){
+		if (isdigit(input[i])) {
+			int num = atoi(input + i);
+			printf("%d ", num);
+
+			while (isdigit(input[i])) ++i;
+			--i;
+
+		} else if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/' || input[i] == '(' || input[i] == ')') {
+			printf("%c ", input[i]);
+	        }
 	}
 }
 
@@ -44,22 +46,15 @@ void getString(char *string, int size){
 
 	int lenght = strlen(string);
 
-	if (lenght > 0 && string[lenght-1] == '\n') string[lenght-1] = '\0';
+	if(lenght > 0 && string[lenght-1] == '\n') string[lenght-1] = '\0';
 }
 
 int main(void){
 	char input[100];
 	getString(input, sizeof(input));
-	
-	int n = strlen(input);
-	double num;
-	int tab[n];
-	getType(input, tab, n);
 
 	printf("%s\n%d\n", input, strlen(input));
-	for (int i = 0; i < n; i++){
-		printf("%d\n", tab[i]);
-	}
+	parseString(input);
 
 
 	return 0;
